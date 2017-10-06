4e8814b89e44a130ad72d0fe15ed21fbbca16f33
Amer Harb <amer.harb@gmail.com>
2017-10-06 17:04:16 +0200

    this code pass untill testcase 09 at hacker rank

--- Java/Absolute Element Sums/src/AbsoluteElementSums.java
+++ Java/Absolute Element Sums/src/AbsoluteElementSums.java
@@ -23,56 +23,98 @@ public class AbsoluteElementSums {
         }
 
         Arrays.sort(a);
-        int fp = getFirst(a, 0); //get first positive value in the array ;
+        aValues[] aa = aggregateRepeating(a);
+        a = null; //empty memory
+
+        int fp = getFirst(aa, new aValues(0, 0)); //get first positive value in the array ;
         long totalP = 0, totalN = 0; //total X, P, N
         for (int i = 0; i < fp; i++) {
-            totalN += -a[i];
+            totalN += -aa[i].total();
         }
-        for (int i = fp; i < a.length; i++) {
-            totalP += a[i];
+        for (int i = fp; i < aa.length; i++) {
+            totalP += aa[i].total();
         }
 
         int totalX = 0;
         for (int i = 0; i < q; i++) {
             totalX += x[i];
-            int newFP = getFirst(a, -totalX);
+            int newFP = getFirst(aa, new aValues(-totalX, 0));
             long total = 0;
+            int NC; //Negative count
+            if (fp > 0) {
+                NC = aa[fp - 1].rs;
+            } else {
+                NC = 0;
+            }
 
-            total += totalP + totalN + totalX * (a.length - fp - newFP);
+            int nNC;//new Negative count
+            if (newFP > 0) {
+                nNC = aa[newFP - 1].rs;
+            } else {
+                nNC = 0;
+            }
+
+            total += totalP + totalN + ((long) totalX * (long) (aa[aa.length - 1].rs - NC - nNC));
 
             for (int j = newFP; j < fp; j++) {
-                total -= Math.abs(a[j]);
-                total += Math.abs(a[j] + totalX);
+                total -= Math.abs(aa[j].total());
+                total += Math.abs(aa[j].total() + (totalX * aa[j].r));
             }
             for (int j = fp; j < newFP; j++) {
-                total -= Math.abs(a[j]);
-                total += Math.abs(a[j] + totalX);
+                total -= Math.abs(aa[j].total());
+                total += Math.abs(aa[j].total() + (totalX * aa[j].r));
             }
 
             System.out.println(total);
-
         }
     }
 
-    static int getFirst(int[] arr, int v) {
+    static int getFirst(aValues[] arr, aValues v) {
         int k = Arrays.binarySearch(arr, v);
         if (k < 0) {
             return -k - 1;
         } else {
-            int b = Arrays.binarySearch(arr, 0, k, v - 1);
-            if (b < 0) {
-                return -b - 1;
-            } else {
-                while (k > 0) {
-                    if (arr[k - 1] == v) {
-                        k--;
-                    } else {
-                        return k;
-                    }
-                }
-                return k;
-            }
+            return k;
         }
     }
 
+    static aValues[] aggregateRepeating(int[] arr) {
+
+        aValues[] t = new aValues[Math.min(arr.length, 4001)];
+
+        int j = 0;
+        t[j] = new aValues(arr[0], 0);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                t[j].r++;
+                t[j].rs++;
+            } else {
+                t[++j] = new aValues(arr[i], t[j - 1].rs);
+            }
+        }
+        aValues[] r = new aValues[j + 1];
+        System.arraycopy(t, 0, r, 0, j + 1);
+        return r;
+    }
+
+    private static class aValues implements Comparable<aValues> {
+
+        private final int a;
+        int r = 1;
+        int rs;
+
+        public int total() {
+            return a * r;
+        }
+
+        public aValues(int a, int prevRS) {
+            this.a = a;
+            this.rs = prevRS + 1;
+        }
+
+        @Override
+        public int compareTo(aValues o) {
+            return this.a - o.a;
+        }
+    }
 }
