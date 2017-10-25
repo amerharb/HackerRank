9af780b473433ab028509f8990b29363ee9b3555
Amer Harb <amer.harb@gmail.com>
2017-10-25 23:43:47 +0200

    fater way fixed array

--- Java/Absolute Element Sums/src/AbsoluteElementSums.java
+++ Java/Absolute Element Sums/src/AbsoluteElementSums.java
@@ -2,71 +2,115 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class AbsoluteElementSums {
+public class AbsoluteElementSums
+{
 
     public static void main(String[] args) {
+        int n;
+        int[] a;
+        int q;
+        int[] x;
+
         Scanner in = new Scanner(System.in);
-
-        int n = in.nextInt();
-        long a[] = new long[n];
+        n = in.nextInt();
+        a = new int[n];
         for (int i = 0; i < n; i++) {
-            a[i] = in.nextLong();
-        }
-        Arrays.sort(a);
-        int fp = getFirst(a, 0L); //get first positive value in the array ;
-        long totalP = 0, totalN = 0; //total X, P, N
-        for (int i = 0; i < fp; i++) {
-            totalN += -a[i];
-        }
-        for (int i = fp; i < a.length; i++) {
-            totalP += a[i];
+            a[i] = in.nextInt();
         }
 
-        int q = in.nextInt();
-        int x[] = new int[q];
+        q = in.nextInt();
+        x = new int[q];
         for (int i = 0; i < q; i++) {
             x[i] = in.nextInt();
         }
 
-        long totalX = 0;
+        FixedArray FA = new FixedArray(a);
+        a = null; //empty memory
+
+        final int NC = FA.getNegativeCount();
+        final int PC = FA.getPositiveCount();
+        final long totalPN = FA.getTotalPositive() - FA.getTotalNegative();
+        int totalX = 0;
+
         for (int i = 0; i < q; i++) {
             totalX += x[i];
-            int newFP = getFirst(a, -totalX);
-            long total = 0;
-
-            total += totalP + (totalX * (a.length - fp));
-            total += totalN - (totalX * fp);
-
-            for (int j = newFP; j < fp; j++) {
-                total -= Math.abs(a[j]);
-                total += totalX;
-                total += Math.abs(a[j] + totalX);
-            }
-            for (int j = fp; j < newFP; j++) {
-                total -= Math.abs(a[j]);
-                total -= totalX;
-                total += Math.abs(a[j] + totalX);
-            }
-
+            int nNC = FA.getNegativeCount(totalX);//new Negative count
+            long total = totalPN + ((long) totalX * (long) (PC - nNC)) + (FA.getTotalNegative() - FA.getTotalSum(-totalX - 1)) * 2 + (long) totalX * (long) (FA.getNegativeCount() - FA.getNegativeCount(totalX));
             System.out.println(total);
-            
         }
     }
 
-    static int getFirst(long[] arr, long v) {
-        int k = Arrays.binarySearch(arr, v);
-        if (k < 0) {
-            return -k - 1;
-        } else {
-            while (k > 0) {
-                if (arr[k - 1] == v) {
-                    k--;
-                } else {
-                    return k;
-                }
+    private static class FixedArray
+    {
+       final int[] r = new int[4001];
+        final int[] rs = new int[4001]; //Repeat Sum
+        final int[] t = new int[4001];
+        final int[] ts = new int[4001]; //totalSum
+
+        private FixedArray(int[] arr) {
+            for (int a : arr) {
+                r[a + 2000]++;
             }
-            return k;
+
+            //update values
+            rs[0] = r[0];
+            t[0] = -2000 * r[0];
+            ts[0] = t[0];
+            for (int i = 1; i < 4001; i++) {
+                rs[i] = r[i] + rs[i - 1];
+                t[i] = (i - 2000) * r[i];
+                ts[i] = t[i] + ts[i - 1];
+            }
+        }
+
+        int getTotal(int num) {
+            return t[num - 2000];
+        }
+
+        int getTotalSum(int num) {
+            if (num < -2000) {
+                return 0;
+            } else if (num > 2000) {
+                return ts[4000];
+            } else {
+                return ts[num + 2000];
+            }
+        }
+
+        int getRepeateSum(int num) {
+            if (num < -2000) {
+                return 0;
+            } else if (num > 2000) {
+                return rs[4000];
+            } else {
+                return rs[num + 2000];
+            }
+        }
+
+        int getTotalNegative() {
+            return ts[1999];
+        }
+
+        int getTotalPositive() {
+            return ts[4000] - ts[1999];
+        }
+
+        private int getNegativeCount() {
+            return rs[1999];
+        }
+
+        private int getPositiveCount() {
+            return rs[4000] - rs[1999];
+        }
+
+        private int getNegativeCount(int offSet) {
+            if (offSet > 1999) {
+                return 0;
+            } else if (offSet < -2001) {
+                return rs[4000];
+            } else {
+                return rs[1999 - offSet];
+            }
         }
     }
-
 }
