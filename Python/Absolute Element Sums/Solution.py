2c6ca499146d6511b45a738710ebe0c8a1432dcf
Amer Harb <amer.harb@gmail.com>
2017-10-31 15:56:55 +0100

    the code is all mistake

--- Python/Absolute Element Sums/Solution.py
+++ Python/Absolute Element Sums/Solution.py
@@ -1,3 +1,21 @@
+def getNegativeCount(offSet):
+    if (offSet > 1999):
+        return 0
+    elif (offSet < -2001):
+        return rs[4000]
+    else:
+        return rs[1999 - offSet]
+
+
+def getTotalSum(offSet):
+    if (offSet < -2000):
+        return 0
+    elif (offSet > 2000):
+        return ts[4000]
+    else:
+        return ts[offSet + 2000]
+
+
 n = int(input())
 a = list(map(int, input().split()))
 q = int(input())
@@ -19,3 +37,15 @@ for i in range(1, 4000):
     rs[i] = r[i] + rs[i - 1]
     t[i] = (i - 2000) * r[i]
     ts[i] = t[i] + ts[i - 1]
+
+a = None
+
+totalPN = ts[4000] - (2 * ts[1999])
+totalX = 0
+
+for i in x:
+    totalX += i
+    tot = totalPN + (totalX * (rs[4000] - rs[1999] - getNegativeCount(totalX)))
+    tot += (ts[1999] - getTotalSum(-totalX - 1)) * 2
+    tot += totalX * (rs[1999] - getNegativeCount(totalX))
+    print(tot)
