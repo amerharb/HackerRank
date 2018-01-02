b678cc8de03e8e0b1c3d2d53a08d6b01c3450446
Amer Harb <amer.harb@gmail.com>
2018-01-02 21:16:51 +0100

    working but it takes time

--- Java/The Coin Change Problem/src/com/hackerrank/Solution.java
+++ Java/The Coin Change Problem/src/com/hackerrank/Solution.java
@@ -1,47 +1,15 @@
 package com.hackerrank;
 
-/**
- * Created by Amer on 28-Dec-17.
- */
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
-// https://www.hackerrank.com/challenges/coin-change/problem
 
+/**
+ * Created by Amer on 02-Jan-18.
+ */
 public class Solution {
 
-	static class CoinUse {
-		long value;
-		long use;
-		final long total;
-
-		CoinUse(long value, long use) {
-			this.value = value;
-			this.use = use;
-			total = value * use;
-		}
-
-	}
-
-	static class CoinUseList extends ArrayList<CoinUse> implements Comparator<CoinUseList> {
-		long value;
-
-		CoinUseList(long value) {
-			this.value = value;
-		}
-
-		@Override
-		public int compare(CoinUseList o1, CoinUseList o2) {
-			if (o1.value > o2.value)
-				return 1;
-			else if (o1.value < o2.value)
-				return -1;
-			else
-				return 0;
-		}
-	}
+	private int col;//current column working
+	private static int curCol = 0;
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -59,56 +27,122 @@ public class Solution {
 	static long getWays(long n, long[] c) {
 		long ways = 0;
 
-		List<CoinUseList> coinList = new ArrayList<>();
-		for (long v : c) {
-			CoinUseList useList = new CoinUseList(v);
-			coinList.add(useList);
-			for (long i = 0; n >= (i * v); i++) {
-				useList.add(new CoinUse(v, i));
-			}
-		}
+		Arrays.sort(c);
+		int[] idx = new int[c.length];
+		int[] idxMax = getIdxMax(idx, c, n);
 
-		int ser[] = new int[coinList.size()];
-		long possTry = 1;
-		int i = 0;
-		for (List l : coinList) {
-			possTry *= l.size();
-			ser[i] = l.size();
-			i++;
-		}
-
-		for (int j = 0; j < possTry; j++) {
-			int[] nb = getNBArray(j, ser);
-			long total = 0;
-			for (int k = 0; k < nb.length; k++) {
-				CoinUse cu = coinList.get(k).get(nb[k]);
-				total += cu.total;
-			}
-			if (total == n) {
+		while (curCol < c.length) {
+			long total = getTotal(idx, c);
+			if (total < n) {
+				incIdx(idx, c, idxMax);
+			} else if (total == n) { //found total
 				ways++;
+				skipFirstCol(idx, c, idxMax);
+
+			} else { // total > n
+				int colToSkip = -1;
+				for (int i = 0; i <= curCol; i++) {
+					int[] tempIdx = new int[idx.length];
+					for (int j = 0; j < i; j++) {
+						tempIdx[j] = 0;
+					}
+					if (idx[i] < idxMax[i]) {
+						tempIdx[i] = idx[i] + 1;
+					} else {
+						continue;
+					}
+					for (int j = i + 1; j < idx.length; j++) {
+						tempIdx[j] = idx[j];
+					}
+
+					if (getTotal(tempIdx, c) > n) {
+						//skipForCol(idx, c, idxMax, i);
+						colToSkip++;
+					} else {
+						break;
+					}
+				}
+				skipForCol(idx, c, idxMax, colToSkip);
 			}
 		}
-
 		return ways;
 	}
 
-	private static int[] getNBArray(int j, int[] ser) {
-		int[] nb = new int[ser.length];
-		for (int i = nb.length - 1; i >= 0 && j > 0; i--) {
-			int max = getMaxSer(i, ser);
-			nb[i] = j / max;
-			j -= (nb[i] * max);
+	private static void skipForCol(int[] idx, long[] c, int[] idxMax, int col) {
+		for (int i = col; i >= 0; i--) {
+			idx[i] = idxMax[i];
 		}
-		return nb;
+		incIdx(idx, c, idxMax);
 	}
 
-	private static int getMaxSer(int i, int[] ser) {
-		int max = 1;
-		for (i--; i >= 0; i--) {
-			max *= ser[i];
+	private static void skipCurCol(int[] idx, long[] c, int[] idxMax) {
+		for (int i = curCol - 1; i >= 0; i--) {
+			idx[i] = idxMax[i];
+		}
+		incIdx(idx, c, idxMax);
+	}
+
+	private static int[] getIdxMax(int[] idx, long[] c, long n) {
+		int[] max = new int[idx.length];
+		for (int i = 0; i < idx.length; i++) {
+			max[i] = (int) (n / c[i]);
 		}
 		return max;
 	}
 
-}
+	private static void incIdx(int[] idx, long[] c, int[] idxMax) {
+		int col = 0;
+		while (col < c.length) {
+			if (idx[col] < idxMax[col]) {
+				idx[col]++;
+				break;
+			} else {
+				idx[col] = 0;
+				col++;
+			}
+		}
+		if (col > curCol) {
+			curCol = col;
+		}
+	}
 
+	private static void skipFirstCol(int[] idx, long[] c, int[] idxMax) {
+		idx[0] = idxMax[0];
+		incIdx(idx, c, idxMax);
+
+		/*
+		idx[0] = 0;
+		int col = 1;
+		while (col < c.length) {
+			if (idx[col] < idxMax[col]) {
+				idx[col]++;
+				break;
+			} else {
+				idx[col] = 0;
+				col++;
+			}
+		}
+		if (col > curCol) {
+			curCol = col;
+		}
+*/
+	}
+
+	private static long getTotal(int[] idx, long[] c) {
+		long total = 0;
+		for (int i = 0; i < c.length; i++) {
+			total += c[i] * idx[i];
+		}
+		return total;
+	}
+
+//	private static int getCurCol(int[] idx) {
+//		for (int i = idx.length - 1; i >= 0; i--) {
+//			if (idx[i] != 0) {
+//				return i;
+//			}
+//		}
+//		return 0;
+//	}
+
+}
--- Java/The Coin Change Problem/src/com/hackerrank/Solution_old.java
+++ Java/The Coin Change Problem/src/com/hackerrank/Solution_old.java
@@ -1,213 +1,62 @@
 package com.hackerrank;
 
-// https://www.hackerrank.com/challenges/coin-change/problem
+/**
+ * Created by Amer on 28-Dec-17.
+ */
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+// https://www.hackerrank.com/challenges/coin-change/problem
 
 public class Solution_old {
 
-	static long getWays(long n, long[] c) {
-		long ways = 0;
+	static class CoinUse implements Comparable<CoinUse> {
+		long value;
+		long use;
+		final long total;
 
-		for (int numberOfCoins = 1; numberOfCoins <= c.length; numberOfCoins++) {
-			long[][] arr = getArrayCombination(c, numberOfCoins);
-			ROW:
-			for (long[] ar : arr) {
-				if (getArrayElimantsSum(ar) <= n) {
-					//calc max time of use the coin
-					int[] maxUse = new int[ar.length];
-					for (int i = 0; i < ar.length; i++) {
-						maxUse[i] = (int) (n / ar[i]);
-//						for (int j = 1; n < j * ar[i]; j++) {
-//							maxUse[i]++;
-//						}
-					}
-
-					//generate array of all possiable way
-					int[][] possComb = new int[getArrayElimantsSumInt(maxUse)][3];
-					int x = 0;
-					for (int i = 0; i < ar.length; i++) {
-						for (int j = 1; j <= maxUse[i]; j++) {
-							possComb[x][0] = (int) ar[i];
-							possComb[x][1] = j;
-							possComb[x][2] = possComb[x][0] * possComb[x][1];
-							x++;
-						}
-					}
-
-					int[][] indexComb = getArrayOfIndexCombination(possComb.length);
-					for (int i = 0; i < indexComb.length; i++) {
-						long total = 0;
-						for (int j = 0; j < indexComb[i].length; j++) {
-							total += possComb[indexComb[i][j]][2];
-						}
-						if (total == n) {
-							ways++;
-						}
-					}
-				}
-			}
-		}
-		return ways;
-	}
-
-	static long[][] getArrayCombination(long[] arr, int k) {
-
-		int pComb = getCombination(arr.length, k);
-
-		long[][] r = new long[pComb][k];
-
-		int[] s = new int[k];
-		for (int i = 0; i < k; i++) {
-			s[i] = i;
-		}// generate first indicate array
-		for (int l = 0; l < k; l++) {
-			r[0][l] = arr[s[l]];
+		CoinUse(long value, long use) {
+			this.value = value;
+			this.use = use;
+			total = value * use;
 		}
 
-		for (int j = 1; j < pComb; j++) {
-			int i;
-			for (i = k - 1; i >= 0 && s[i] == arr.length - k + i; i--) ;
-			if (i < 0) {
-				break;
-			}
-
-			s[i]++;                    // increment this item
-			for (i++; i < k; i++) {    // fill up remaining items
-				s[i] = s[i - 1] + 1;
-			}
-
-			for (int l = 0; l < k; l++) {
-				r[j][l] = arr[s[l]];
-			}
-
-
-		}
-		return r;
-	}
-
-	static int getCombination(int n, int k) {
-		if (k > n) {
-			return 0;
-		} else if (k == 0) {
-			return 1;
-		} else {
-			return getLimitedFactorial(n, k + 1) / getLimitedFactorial((n - k), 1);
+		@Override
+		public int compareTo(CoinUse o) {
+			if (this.value > o.value)
+				return 1;
+			else if (this.value < o.value)
+				return -1;
+			else if (this.use > o.use)
+				return 1;
+			else if (this.use < o.use)
+				return -1;
+			else
+				return 0;
 		}
 	}
 
-	private static int getLimitedFactorial(int s, int e) {
-		if (s < e) {
-			return 1;
-		} else if (s == e) {
-			return s;
-		} else {
-			return s * getLimitedFactorial(s - 1, e);
-		}
-	}
+	static class CoinUseList extends ArrayList<CoinUse> implements Comparable<CoinUseList> {
+		long value;
 
-	static int[][] getArrayOfIndexCombination(int numberOfElements) {
-		int[][][] r = new int[numberOfElements + 1][][];
-
-		for (int i = 0; i <= numberOfElements; i++) {
-			int pComb = getCombination(numberOfElements, i);
-			r[i] = getArrayOfIndexCombinationForK(numberOfElements, i);
+		CoinUseList(long value) {
+			this.value = value;
 		}
 
-		return get2DArrayFrom3D(r);
-	}
-
-	static int[][] get2DArrayFrom3D(int[][][] arr) {
-		int[][] r;
-		int c = 0;
-		//TODO: check if the array length is enough
-		int[][] temp = new int[Math.max(arr.length * arr[0].length, 1000)][];//estimation can be increase
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++) {
-				temp[c] = arr[i][j];
-				c++;
-			}
+		@Override
+		public int compareTo(CoinUseList o) {
+			if (this.value > o.value)
+				return 1;
+			else if (this.value < o.value)
+				return -1;
+			else
+				return 0;
 		}
-		r = new int[c][];
-		System.arraycopy(temp, 0, r, 0, c);
-		return r;
-	}
-
-	static int[][] getArrayOfIndexCombinationForK(int n, int k) {
-		List<int[]> subsets = new ArrayList<>();
-
-		int[] s = new int[k];// here we'll keep indices
-		// pointing to elements in input array
-
-		if (k <= 0) {
-			return new int[1][0];
-		} else if (k <= n) {
-			// first index sequence: 0, 1, 2, ...
-			for (int i = 0; (s[i] = i) < k - 1; i++) ;
-			subsets.add(getArrayCopy(s));
-			for (; ; ) {
-				int i;
-				// find position of item that can be incremented
-				for (i = k - 1; i >= 0 && s[i] == n - k + i; i--) ;
-				if (i < 0) {
-					break;
-				}
-				s[i]++;                    // increment this item
-				for (++i; i < k; i++) {    // fill up remaining items
-					s[i] = s[i - 1] + 1;
-				}
-				subsets.add(getArrayCopy(s));
-			}
-			int[][] r = new int[subsets.size()][];
-			int i = 0;
-			for (int[] a : subsets) {
-				r[i] = a;
-				i++;
-			}
-			return r;
-		}
-		return null;
-	}
-
-	static int[] getArrayCopy(int[] arr) {
-		int[] a = new int[arr.length];
-		System.arraycopy(arr, 0, a, 0, a.length);
-		return a;
-	}
-
-	static long getArrayElimantsSum(long[] arr) {
-		long s = 0;
-		for (long l : arr) {
-			s += l;
-		}
-		return s;
-	}
-
-	static int getArrayElimantsSumInt(int[] arr) {
-		int s = 0;
-		for (int i : arr) {
-			s += i;
-		}
-		return s;
 	}
 
 	public static void main(String[] args) {
-//		System.out.println(getLimitedFactorial(4, 5));
-//		System.out.println(getCombination(10, 3));
-//
-//		long[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
-//		//long[][] r = getArrayCombination(arr, 2);
-//		int[][] r = getArrayOfIndexCombination(9);
-//		for (int[] ro : r) {
-//			for (int roo : ro) {
-//				System.out.print(roo + ", ");
-//			}
-//			System.out.println();
-//		}
-//		int rr[][] = getArrayOfIndexCombination(9);
-//
-//		System.exit(0);
-
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int m = in.nextInt();
@@ -218,5 +67,145 @@ public class Solution_old {
 		// Print the number of ways of making change for 'n' units using coins having the values given by 'c'
 		long ways = getWays(n, c);
 		System.out.println(ways);
+	}
+
+	static long getWays(long n, long[] c) {
+		long ways = 0;
+
+//		Comparator<CoinUse> coinUseComparator = new Comparator<CoinUse>() {
+//			@Override
+//			public int compare(CoinUse o1, CoinUse o2) {
+//				//bigger value first
+//				if (o1.value > o2.value)
+//					return -1;
+//				else if (o1.value < o2.value)
+//					return 1;
+//				else if (o1.use > o2.use)
+//					return -1;
+//				else if (o1.use < o2.use)
+//					return 1;
+//				else
+//					return 0;
+//			}
+//		};
+//
+//		List<CoinUseList> coinList = new ArrayList<>();
+//		for (long v : c) {
+//			CoinUseList useList = new CoinUseList(v);
+//			coinList.add(useList);
+//			for (long i = 0; n >= (i * v); i++) {
+//				useList.add(new CoinUse(v, i)); //38-61429-57
+//			}
+//			useList.sort(coinUseComparator);
+//		}
+
+		List<CoinUse> coinUseList= new ArrayList<>();
+		for (long v : c) {
+			for (long i = 0; n >= (i * v); i++) {
+				coinUseList.add(new CoinUse(v, i));
+			}
+		}
+		Comparator<CoinUse> coinUseTotalComparator= new Comparator<CoinUse>() {
+			@Override
+			public int compare(CoinUse o1, CoinUse o2) {
+				//bigger value first
+				if (o1.total > o2.total)
+					return -1;
+				else if (o1.total < o2.total)
+					return 1;
+				else
+					return 0;
+			}
+		};
+		coinUseList.sort(coinUseTotalComparator);
+
+//		Comparator<CoinUseList> coinUseListComparator = new Comparator<CoinUseList>() {
+//			@Override
+//			public int compare(CoinUseList o1, CoinUseList o2) {
+//				//biger value first
+//				if (o1.value > o2.value)
+//					return -1;
+//				else if (o1.value < o2.value)
+//					return 1;
+//				else
+//					return 0;
+//			}
+//		};
+//
+//		coinList.sort(coinUseListComparator);
+
+//		int CoinUseCount = 0;
+//		for (CoinUseList cul : coinList) {
+//			CoinUseCount += cul.size();
+//		}
+//		CoinUse[] CoinUseArr = new CoinUse[CoinUseCount];
+//		CoinUseCount = 0;
+//		for (CoinUseList cul : coinList) {
+//			for (CoinUse cu : cul) {
+//				CoinUseArr[CoinUseCount++] = cu;
+//			}
+//		}
+
+		//TRIAL:
+		//int find = pairMe(0, coinUseList, 0, n);
+		//System.out.println(find);
+//		long ser[] = new long[coinList.size()];
+//		long serMax[] = new long[coinList.size()];
+//
+////		long possTry = 1;
+////		int i = 0;
+////		serMax[0] = 1;
+////		for (List cu : coinList) {
+////			possTry *= cu.size();
+////			ser[i] = cu.size();
+////			if (i > 0)
+////				serMax[i] = serMax[i - 1] * ser[i - 1];
+////			i++;
+////		}
+////
+////		for (int j = 0; j < possTry; j++) {
+////			int[] nb = getNBArray(j, serMax);
+////			long total = 0;
+////			for (int k = 0; k < nb.length; k++) {
+////				CoinUse cu = coinList.get(k).get(nb[k]);
+////				total += cu.total;
+////				if (total > n) {
+////					break;
+////				}
+////			}
+////			if (total == n) {
+////				ways++;
+////			}
+////			if (j % 10_000_000 == 0)
+////				System.out.println("10 Million, ways found so far: " + ways + " finish " + (float) j / possTry);
+////		}
+
+		return ways;
+	}
+
+	private static int pairMe(int index, ArrayList<CoinUse> list, long total, long target) {
+		for (int i = index; i < list.size(); i++) {
+
+		}
+		return -1;
+	}
+
+	private static int[] getNBArray(int j, long[] ser) { //ser changed to serMax in calling
+		int[] nb = new int[ser.length];
+		for (int i = nb.length - 1; i >= 0 && j > 0; i--) {
+//			int max = getMaxSer(i, ser);
+			long max = ser[i];
+			nb[i] = (int) ((long) j / max);
+			j -= (nb[i] * max);
+		}
+		return nb;
+	}
+
+	private static int getMaxSer(int i, int[] ser) {
+		int max = 1;
+		for (i--; i >= 0; i--) {
+			max *= ser[i];
+		}
+		return max;
 	}
 }
