51d36bbc0d10e04681d16cbede5ecbc5dd49e6b4
Amer Harb <amer.harb@gmail.com>
2018-01-10 18:39:16 +0100

    work locally

--- Java/The Coin Change Problem/src/com/hackerrank/Solution.java
+++ Java/The Coin Change Problem/src/com/hackerrank/Solution.java
@@ -16,71 +16,29 @@ public class Solution {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int m = in.nextInt();
-        long[] c = new long[m];
+        int[] c = new int[m];
         for (int c_i = 0; c_i < m; c_i++) {
-            c[c_i] = in.nextLong();
+            c[c_i] = in.nextInt();
         }
         // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
 
+        long w = System.currentTimeMillis();
         long ways = getWays(n, c); //give the index of last elemint+1
         System.out.println(ways);
+        System.out.println((System.currentTimeMillis() - w) + " mili sec");
     }
 
-    private long getWays(long n, long[] c) {
+    private long getWays(int n, int[] c) {
 
-        Arrays.sort(c);
-        long[] a = new long[c.length];
-        int i = a.length;
+        long[] t = new long[n + 1];
+        t[0] = 1;
 
-        long ways = 0;
-
-
-        while (i != 0) {
-            long sum = n;
-            for (int j = a.length - 1; j > i - 1; j--) {
-                sum -= a[j] * c[j];
-            }
-            for (int j = i - 1; j >= 0; j--) {
-                a[j] = sum / c[j];
-                sum -= a[j] * c[j];
-            }
-            if (sum == 0) {
-                if (c[2] % c[1] == 0 && c[1] % c[0] == 0) {
-                    ways += ((a[1] + 1) * (a[2] + 1)) + (getSerSum(a[2]) * c[1]);
-                    a[2] = 0;
-                    a[1] = 0;
-                } else if (c[1] % c[0] == 0) {
-                    ways += a[1] + 1;
-                    a[1] = 0;
-                }else{
-                    ways++;
-                }
-            } else {
-                if (c[2] % c[1] == 0 && c[1] % c[0] == 0) {
-                    a[2] = 0;
-                    a[1] = 0;
-                } else if (c[1] % c[0] == 0) {
-                    a[1] = 0;
-                }
-            }
-
-            i = 0;
-            for (int j = 1; j < a.length; j++) {
-                if (a[j] > 0) {
-                    a[j]--;
-                    i = j;
-                    break;
-                }
+        for (int i = 0; i < c.length; i++) {
+            for (int j = c[i]; j < n + 1; j++) {
+                t[j] += t[j - c[i]];
             }
         }
-        return ways;
+        return t[n];
     }
 
-    private long getSerSum(long x) {
-        long t = 0;
-        for (long l = x; l > 0; l--) {
-            t += l;
-        }
-        return t;
-    }
 }
