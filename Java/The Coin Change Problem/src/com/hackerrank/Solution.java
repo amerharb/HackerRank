662c8ff78be2e03a87b249201033fcc8ba2bd4a7
Amer Harb <amer.harb@gmail.com>
2018-01-10 11:01:40 +0100

    enhance the proformance

--- Java/The Coin Change Problem/src/com/hackerrank/Solution.java
+++ Java/The Coin Change Problem/src/com/hackerrank/Solution.java
@@ -22,8 +22,10 @@ public class Solution {
         }
         // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
 
+        long w = System.currentTimeMillis();
         long ways = getWays(n, c); //give the index of last elemint+1
         System.out.println(ways);
+        System.out.println((System.currentTimeMillis() - w) + " mili sec");
     }
 
     private long getWays(long n, long[] c) {
@@ -34,42 +36,73 @@ public class Solution {
 
         long ways = 0;
 
-
-        while (i != 0) {
-            long sum = n;
-            for (int j = a.length - 1; j > i - 1; j--) {
-                sum -= a[j] * c[j];
-            }
-            for (int j = i - 1; j >= 0; j--) {
-                a[j] = sum / c[j];
-                sum -= a[j] * c[j];
-            }
-            if (sum == 0) {
-                if (c[2] % c[1] == 0 && c[1] % c[0] == 0) {
+        if (c[2] % c[1] == 0 && c[1] % c[0] == 0) {
+            while (i != 0) {
+                long sum = n;
+                for (int j = a.length - 1; j > i - 1; j--) {
+                    sum -= a[j] * c[j];
+                }
+                for (int j = i - 1; j >= 0; j--) {
+                    a[j] = sum / c[j];
+                    sum -= a[j] * c[j];
+                }
+                if (sum == 0) {
                     ways += ((a[1] + 1) * (a[2] + 1)) + (getSerSum(a[2]) * c[1]);
-                    a[2] = 0;
-                    a[1] = 0;
-                } else if (c[1] % c[0] == 0) {
-                    ways += a[1] + 1;
-                    a[1] = 0;
-                }else{
-                    ways++;
                 }
-            } else {
-                if (c[2] % c[1] == 0 && c[1] % c[0] == 0) {
-                    a[2] = 0;
-                    a[1] = 0;
-                } else if (c[1] % c[0] == 0) {
-                    a[1] = 0;
+
+                i = 0;
+                for (int j = 3; j < a.length; j++) {
+                    if (a[j] > 0) {
+                        a[j]--;
+                        i = j;
+                        break;
+                    }
                 }
             }
+        } else if (c[1] % c[0] == 0) {
+            while (i != 0) {
+                long sum = n;
+                for (int j = a.length - 1; j > i - 1; j--) {
+                    sum -= a[j] * c[j];
+                }
+                for (int j = i - 1; j >= 0; j--) {
+                    a[j] = sum / c[j];
+                    sum -= a[j] * c[j];
+                }
+                if (sum == 0) {
+                    ways += a[1] + 1;
+                }
 
-            i = 0;
-            for (int j = 1; j < a.length; j++) {
-                if (a[j] > 0) {
-                    a[j]--;
-                    i = j;
-                    break;
+                i = 0;
+                for (int j = 2; j < a.length; j++) {
+                    if (a[j] > 0) {
+                        a[j]--;
+                        i = j;
+                        break;
+                    }
+                }
+            }
+        } else {
+            while (i != 0) {
+                long sum = n;
+                for (int j = a.length - 1; j > i - 1; j--) {
+                    sum -= a[j] * c[j];
+                }
+                for (int j = i - 1; j >= 0; j--) {
+                    a[j] = sum / c[j];
+                    sum -= a[j] * c[j];
+                }
+                if (sum == 0) {
+                        ways++;
+                }
+
+                i = 0;
+                for (int j = 1; j < a.length; j++) {
+                    if (a[j] > 0) {
+                        a[j]--;
+                        i = j;
+                        break;
+                    }
                 }
             }
         }
