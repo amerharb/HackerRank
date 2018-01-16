d5db0bcd132d65c7b184b4070e656ec802d3ee16
Amer Harb <amer.harb@gmail.com>
2018-01-16 12:44:11 +0100

    function getIsland return 2D array contain rows that contain number of cities in each row for each island

--- Java/Roads and Libraries/src/com/hackerrank/Solution.java
+++ Java/Roads and Libraries/src/com/hackerrank/Solution.java
@@ -4,46 +4,74 @@ import java.util.*;
 
 public class Solution {
 
-    static int roadsAndLibraries(int n, int nLib, int nRoad, int[][] cities) {
-        if (nRoad >= nLib) {
-            return n * nLib;
-        } else {
-            int islands = getIslands(n, cities);
-            return islands;
+    static int roadsAndLibraries(int nCities, int nLib, int nRoad, int[][] cities) {
+
+        int[][] islands = getIslands(nCities, cities);
+        for (int i = 0; i < islands.length; i++) {
+            System.out.print("island " + i + " : ");
+            for (int j = 0; j < islands[i].length; j++) {
+                System.out.print(islands[i][j] + ", ");
+            }
+            System.out.println();
         }
+        return 0;
+
+        //        if (nRoad >= nLib) {
+//            return n * nLib;
+//        } else {
+//            int islands = getIslands(n, cities);
+//            return islands;
+//        }
     }
 
-    private static int getIslands(int n, int[][] cities) {
-        boolean[] visitedCity = new boolean[n];
+    private static int[][] getIslands(int n, int[][] cities) {
+        int[] visitedCity = new int[n];
+        int tourCount = 0;
         int islands = 0;
         for (int i = 0; i < n; i++) {
-            if (!visitedCity[i]) {
-                visitedCity[i] = true;
+            if (visitedCity[i] == 0) { //city never visited
+                tourCount++;
+                visitedCity[i] = tourCount;
                 islands++;
                 //try to visit all related
                 for (int j = 0; j < cities.length; j++) {
-                    if (i == cities[j][0]-1) {
-                        travelTo(cities ,visitedCity, cities[j][1]-1);
+                    if (i == cities[j][0] - 1) {
+                        travelTo(cities, visitedCity, cities[j][1] - 1, tourCount);
                     }
-                    if (i == cities[j][1]-1) {
-                        travelTo(cities, visitedCity, cities[j][0]-1);
+                    if (i == cities[j][1] - 1) {
+                        travelTo(cities, visitedCity, cities[j][0] - 1, tourCount);
                     }
                 }
             }
         }
-        return islands;
+
+        int[][] islandArr = new int[islands][];
+        int c = 0;
+        for (int i = 0; i < islands; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < visitedCity.length; j++) {
+                if (visitedCity[j] == i + 1) {
+                    list.add(j + 1);
+                }
+            }
+            islandArr[i] = new int[list.size()];
+            for (int j = 0; j < list.size(); j++) {
+                islandArr[i][j] = list.get(j).intValue();
+            }
+        }
+        return islandArr;
     }
 
-    private static void travelTo(int[][] cities, boolean[] visitedCity, int c) {
-        if (!visitedCity[c]) {
-            visitedCity[c] = true;
+    private static void travelTo(int[][] cities, int[] visitedCity, int c, int tourCount) {
+        if (visitedCity[c] == 0) {
+            visitedCity[c] = tourCount;
             //try to visit all related
             for (int j = 0; j < cities.length; j++) {
-                if (c == cities[j][0]-1) {
-                    travelTo(cities ,visitedCity, cities[j][1]-1);
+                if (c == cities[j][0] - 1) {
+                    travelTo(cities, visitedCity, cities[j][1] - 1, tourCount);
                 }
-                if (c == cities[j][1]-1) {
-                    travelTo(cities, visitedCity, cities[j][0]-1);
+                if (c == cities[j][1] - 1) {
+                    travelTo(cities, visitedCity, cities[j][0] - 1, tourCount);
                 }
             }
         }
